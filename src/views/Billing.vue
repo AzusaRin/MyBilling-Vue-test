e81f55de9fb87f04b0999d5f2056661e884c17b3
Azusa <qq745847188@gmail.com>
2022-07-02 18:25:20 +0800

    全部引用icons，封装Icon组件，供给Nav使用

--- src/views/Billing.vue
+++ src/views/Billing.vue
@@ -1,17 +1,13 @@
 <template>
   <div>
     <layout>
-      <svg>
-        <use xlink:href="#billing"/>
-      </svg>
       <p>Billing</p>
     </layout>
   </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/billing.svg'
-console.log(x)
+
 
 export default {
   name: 'Billing',
