7c0f23fdec3026f5b60a4fc103095d3a03591c75
Azusa <qq745847188@gmail.com>
2022-07-03 18:14:49 +0800

    添加Nav样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="nav">
-    <router-link to="/billing/">
-      <svg >
-       <icon name="billing"/>
+  <nav>
+    <router-link to="/billing/" class="item">
+      <svg class="icon">
+        <icon name="billing"/>
       </svg>
-      记账</router-link>
-    |
-    <router-link to="/labels/">
-      <svg>
+      记账
+    </router-link>
+    <router-link to="/labels/" class="item">
+      <svg class="icon">
         <icon name="labels"/>
       </svg>
-      标签</router-link>
-    |
-    <router-link to="/statistics/">
-      <svg>
+      标签
+    </router-link>
+    <router-link to="/statistics/" class="item">
+      <svg class="icon">
         <icon name="statistics"/>
       </svg>
-      统计</router-link>
-  </div>
+      统计
+    </router-link>
+  </nav>
 
 </template>
 
@@ -30,11 +31,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav svg{
-  width: 1em; height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
+nav {
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
+  font-size: 12px;
+
+  .icon {
+    height: 32px;
+    width: 32px;
+    padding: 2px;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+
+  }
+
+
+  > .item {
+    padding: 5px 0;
+    display: flex;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
 }
 
 </style>
