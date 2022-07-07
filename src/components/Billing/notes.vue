ea82e4996be8baeed5c7ab1bedbf49ac0522728a
Azusa <qq745847188@gmail.com>
2022-07-07 14:32:28 +0800

    暂时写好notes功能

--- src/components/Billing/notes.vue
+++ src/components/Billing/notes.vue
@@ -2,15 +2,22 @@
   <div>
     <label class="notes">
       <span class="noteName">备注</span>
-      <input type="text" placeholder="请在这里输入备注信息">
+      <input type="text" :value="value" @change="onChange" placeholder="请在这里输入备注信息">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'notes'
-};
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class notes extends Vue {
+  value = '';
+  onChange(event:KeyboardEvent){
+    this.value = (event.target as HTMLInputElement).value;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
