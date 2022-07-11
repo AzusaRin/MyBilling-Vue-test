66eac20ba480ef0f2a9ecfbe9093080e0653a5d6
Azusa <qq745847188@gmail.com>
2022-07-11 14:27:30 +0800

    使用TS写model，封装，删掉了数据迁移

--- src/views/Billing.vue
+++ src/views/Billing.vue
@@ -17,33 +17,13 @@ import Notes from '@/components/Billing/notes.vue';
 import Types from '@/components/Billing/types.vue';
 import NumberPad from '@/components/Billing/numberPad.vue';
 import {Component, Watch} from 'vue-property-decorator';
-const {model} = require('@/model.js');
-
-const version = window.localStorage.getItem('version');
-const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-if (version === '0.0.1') {
-  //数据迁移
-  recordList.forEach(record => {
-    record.createAt = new Date(2020, 0, 1);
-  });
-  //保存数据
-  window.localStorage.setItem('recordList', JSON.stringify(recordList));
-}
-//升级版本
-window.localStorage.setItem('version', '1.0.0');
-type Record = {
-  tags: string[]
-  notes: string
-  type: string
-  amount: number
-  createAt?: Date
-}
+import model from '@/model';
 
 @Component({components: {NumberPad, Types, Notes, Tags}})
 export default class Billing extends Vue {
   tags = ['1', '2', '3', '4'];
-  recordList: Record[] = model.fetch();
-  record: Record = {
+  recordList = model.fetch();
+  record: RecordItem = {
     tags: [], type: '-', notes: '', amount: 0
   };
 
@@ -64,7 +44,7 @@ export default class Billing extends Vue {
   }
 
   saveRecord() {
-    const newRecord: Record = JSON.parse(JSON.stringify(this.record));
+    const newRecord = model.clone(this.record);
     newRecord.createAt = new Date();
     this.recordList.push(newRecord);
 
@@ -72,7 +52,7 @@ export default class Billing extends Vue {
 
   @Watch('recordList')
   onRecordListChange() {
-    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    model.save(this.recordList);
   }
 
 }
