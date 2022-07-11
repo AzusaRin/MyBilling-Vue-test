249c3f490e24c1480de9a520924d7d364810746f
Azusa <qq745847188@gmail.com>
2022-07-11 16:55:38 +0800

    新增EditLabel组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,10 +7,18 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
+import tagListModel from '@/models/tagListModel';
 
 @Component
 export default class EditLable extends Vue {
-
+created(){
+const id = this.$route.params.id;
+tagListModel.fetch;
+const tag = tagListModel.data.filter(tag =>tag.id === id)[0]
+  if(tag){
+    console.log(tag)
+  }
+}
 }
 </script>
 
