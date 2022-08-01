26c43ddf04b2ff43779b365889b80fa83c05935d
Azusa <qq745847188@gmail.com>
2022-08-01 12:41:40 +0800

    修改了重置标签的代码

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -13,8 +13,9 @@
           <icon name="createTag"/>
           新建标签
         </DeleteButton>
-        <DeleteButton class="createTag" @click.native="tagClear">
-          <icon name="tagClear"/>
+        <el-divider direction="vertical"></el-divider>
+        <DeleteButton class="createTag" @click.native="tagReset">
+          <icon name="tagReset"/>
           标签初始化
         </DeleteButton>
       </div>
@@ -48,12 +49,37 @@ export default class Labels extends mixins(createTagHelper) {
     }
   }
 
-  tagClear() {
+  tagReset() {
     if (window.confirm('真的要初始化所有标签吗？')) {
-      this.tags.splice(21, 10000);
+
+      this.tags.splice(0, 10000);
+      this.tags.push( {id:'0',name:'交通'},
+          {id:'1',name:'餐饮'},
+          {id:'2',name:'礼金'},
+          {id:'3',name:'学习'},
+          {id:'4',name:'维修'},
+          {id:'5',name:'旅行'},
+          {id:'6',name:'数码'},
+          {id:'7',name:'汽车'},
+          {id:'8',name:'书籍'},
+          {id:'9',name:'烟酒'},
+          {id:'10',name:'社交'},
+          {id:'11',name:'礼物'},
+          {id:'12',name:'办公'},
+          {id:'13',name:'长辈'},
+          {id:'14',name:'孩子'},
+          {id:'15',name:'住房'},
+          {id:'16',name:'美容'},
+          {id:'17',name:'衣服'},
+          {id:'18',name:'电话'},
+          {id:'19',name:'家具'},
+          {id:'20',name:'娱乐'})
       this.$store.commit('saveTags');
     }
   }
+
+
+
 
   created() {
     this.$store.commit('fetchTagList');
@@ -95,9 +121,10 @@ export default class Labels extends mixins(createTagHelper) {
   padding: 16px;
   margin-top: 44-16px;
 
-   > .createTag{
-     margin: 0 20px;
-   }
+  > .createTag{
+    margin: 0 20px;
+  }
+
 }
 
 
